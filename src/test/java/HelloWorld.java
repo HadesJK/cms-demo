593b604591d309f3194cbc093652c2bbd4866ccb
一饭 <yifan@yifan.local>
2016-07-14 13:20:53 +0800

    Hello World

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yifan on 16/7/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello HadesJK!");
+    }
+}
