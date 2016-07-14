a4ba9719110a5128839f3e85fb06c3a3595d1fe8
一饭 <yifan@yifan.local>
2016-07-14 13:54:38 +0800

    correct

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,7 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-       a System.out.println("Hello World!");
+        System.out.println("Hello World!");
         System.out.println("Hello HadesJK!");
     }
 }
