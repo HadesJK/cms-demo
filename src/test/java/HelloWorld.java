b14995a484d2ea6d0f666299a76400878bda782f
一饭 <yifan@yifan.local>
2016-07-14 13:50:04 +0800

    error test

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,7 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       a System.out.println("Hello World!");
         System.out.println("Hello HadesJK!");
     }
 }
